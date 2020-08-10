35e5bb018279cd52b0921a0c40e031a0d36c2b59
jamesesmond <43595340+jamesesmond@users.noreply.github.com>
2020-08-10 11:58:08 -0700

    Add files via upload

--- RuntimeSet.cs
+++ RuntimeSet.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public abstract class RuntimeSet<T> : ScriptableObject
 {
+    //https://github.com/roboryantron/Unite2017
+    //https://www.youtube.com/watch?v=raQ3iHhE_Kk
+
     public List<T> Items = new List<T>();
 
     public void Add(T thing)
